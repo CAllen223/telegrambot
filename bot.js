3ea585b05bc56091d95dab43c1b4cdb97417f795
christopher allen <christopher.allen@tuni.fi>
2021-01-12 17:29:59 +0200

    everytihng up to date

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.on('fredag', (ctx) => {
+bot.on('/fredag', (ctx) => {
     // resend existing file by file_id
     ctx.replyWithSticker('AADBAADCwADQH0BFCXkUVUYJnY1Ag')})
     
